e18cf8a1713aff67181765eeb62660dbb55d5143
emclaug2 <evan.w.mclaughlin@gmail.com>
2019-09-16 15:40:57 -0400

    Added react and angular demos.

---
+++ demo-react/src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+// @ts-ignore
+import ReactHighcharts from 'react-highcharts';
+
+// @ts-ignore
+import { createLineChart, createBarChart, createPieChart, createDonutChart } from '@pxblue/highcharts';
+
+// @ts-ignore
+import * as PXBColors from '@pxblue/colors';
+
+const mainConfig = {
+   colors: [PXBColors.blue[900], PXBColors.blue[500], PXBColors.blue[200]]
+};
+const lineConfig = {
+   colors: [PXBColors.blue[500], PXBColors.blue[200], PXBColors.blue[200]]
+};
+
+const App: React.FC = () => {
+   return (
+      <div>
+         <div style={{height: '200px'}}>
+            <ReactHighcharts config={createPieChart()}/>
+         </div>
+         <div style={{height: '300px'}}>
+            <ReactHighcharts config={createDonutChart(mainConfig)}/>
+         </div>
+         <div style={{height: '400px'}}>
+            <ReactHighcharts config={createLineChart(lineConfig)}/>
+         </div>
+         <div style={{height: '400px'}}>
+            <ReactHighcharts config={createBarChart(mainConfig)}/>
+         </div>
+      </div>
+   );
+}
+
+export default App;
